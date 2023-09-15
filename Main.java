7acef2f8148780ee1499ca4553b0da7cf0c78c80
Ben Fahrmeier <144169391+BKfarm@users.noreply.github.com>
2023-09-15 10:19:10 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,43 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intOperandA = 4;
+        int intOperandB = 7;
+
+        int intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        int intProduct = intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        int intDifference = intOperandA - intOperandB;
+        System.out.println("The difference between " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        int intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+        int intModulo = intOperandA % intOperandB;
+        System.out.println("The remainder of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+
+        double doubleOperandA = 3.14;
+        double doubleOperandB = 3.5;
+
+        double doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+        double doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+        double doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference between " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+        double doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+        double doubleModulo = doubleOperandA % doubleOperandB;
+        System.out.println("The remainder of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleModulo);
+    
+    }
+}
